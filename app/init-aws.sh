5436f0f8ae270a0e4097a953b3b909b5ad85fd70
Pablo Díaz <pablodiaz@citibox.com>
2026-03-18 17:14:43 +0100

    Make the script executable

--- app/init-aws.sh
+++ app/init-aws.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-awslocal s3 mb s3://user-avatars
 
-awslocal dynamodb create-table \
-    --table-name users-table \
-    --attribute-definitions AttributeName=email,AttributeType=S) \
+# Create S3 bucket
+echo "Creating S3 bucket: $S3_BUCKET_NAME"
+aws s3 mb s3://"$S3_BUCKET_NAME" --endpoint-url="$AWS_ENDPOINT_URL"
+
+# Create DynamoDB table
+echo "Creating DynamoDB table: $DYNAMODB_TABLE_NAME"
+aws dynamodb create-table \
+    --table-name "$DYNAMODB_TABLE_NAME" \
+    --attribute-definitions AttributeName=email,AttributeType=S \
     --key-schema AttributeName=email,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --endpoint-url="$AWS_ENDPOINT_URL"
+
+echo "Localstack resources initialized."
